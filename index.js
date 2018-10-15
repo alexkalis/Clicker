55b3791b2eca2ddb9a8efe17f62a4acad0170fc0
Alex Kalis <99039736@mydavinci.nl>
2018-10-15 13:48:35 +0200

    Added a tab system with upgrades and a shop. Added a check if you have enough money. And added a clicker multiplier.

--- index.js
+++ index.js
@@ -3,84 +3,67 @@ let numbers = 0;
 let workers = 0;
 let grandmas = 0;
 let bankers = 0;
+let multiplier = 0;
 document.getElementById("settings").style.visibility = "hidden";
+document.getElementById("info").style.visibility = "hidden";
+
 //saving the game in local storage
-function saveData() {
-    var save = {
-        numbers : numbers,
-        workers: workers,
-        grandmas : grandmas
-    }
- localStorage.setItem("save", JSON.stringify(save));
- console.log('saved');
-}
-//listens to a browser closing or reloading and calls the save function
-window.addEventListener("beforeunload", function(e){
-    saveData();
-}, false);
 
-//checks if the ctrl + s button are entered and then calls the save function
-var isCtrl = false;$(document).keyup(function (e) {
-if(e.which == 17) isCtrl=false;
-}).keydown(function (e) {
-    if(e.which == 17) isCtrl=true;
-    if(e.which == 83 && isCtrl == true) {
-        var save = {
-            numbers : numbers,
-            workers: workers,
-            grandmas : grandmas
-        }
-        alert('saved');
-        localStorage.setItem("save", JSON.stringify(save));
 
- 	return false;
- }
-});
-
-/*this is a function that loads the saved data from the localstorage and puts the data into the variables declared up above.
-*this function is automatically called when the browser is reloaded
-*/
-(function loadData() {
-const savegame = JSON.parse(localStorage.getItem("save"));
-if (typeof savegame.numbers !== "undefined") numbers = savegame.numbers;
-if (typeof savegame.workers !== "undefined") workers = savegame.workers;
-if (typeof savegame.grandmas !== "undefined") grandmas = savegame.grandmas;
-console.log('loaded');
-})();
-/*
-*this function deletes the data from the localStorage
-*/
-function deleteData() {
-    localStorage.removeItem("save");
-    console.log('save deleted');
-}
 
 function settings() {
     document.getElementById("settings").style.visibility = "visible";
-    document.getElementById("settings").style.height = "450px";
-    document.getElementById("settings").style.width = "1265px";
+    document.getElementById("allUpgrades").style.visibility = "visible";
+    document.getElementById("money").style.visibility = "visible";
 
-    document.getElementById("all").style.visibility = "hidden";
+    document.getElementById("extras").style.visibility = "hidden";
+    document.getElementById('info').style.visibility = "hidden";
+
     document.getElementById("settingsButton").setAttribute("onclick","closeSettings()");
 }
 function closeSettings() {
+    document.getElementById("extras").style.visibility = "visible";
 
     document.getElementById("settings").style.visibility = "hidden";
-    document.getElementById("all").style.visibility = "visible";
+    document.getElementById('info').style.visibility = "hidden";
+
     document.getElementById("settingsButton").setAttribute("onclick","settings()");
+
+}
+function info() {
+    document.getElementById("settings").style.visibility = "hidden";
+
+    document.getElementById("money").style.visibility = "visible";
+    document.getElementById("extras").style.visibility = "hidden";
+    document.getElementById("allUpgrades").style.visibility = "visible";
+    document.getElementById('info').style.visibility = "visible";
+
+    document.getElementById("infoButton").setAttribute("onclick","closeInfo()");
+}
+function closeInfo() {
+    document.getElementById("settings").style.visibility = "hidden";
+    document.getElementById("info").style.visibility = "hidden";
+
+    document.getElementById("extras").style.visibility = "visible";
+
+    document.getElementById("infoButton").setAttribute("onclick","info()");
 }
 /*
 *when this function is called it adds a number to the number variable
 *and then updates the number element to show the different number
 */
-function clicker(num) {
+function autoClicker(num) {
     numbers = numbers + num;
     document.getElementById('number').innerHTML = numbers;
-    // console.log(numbers);
-    var money = document.getElementById('animation');
-    // shake($('.moneySign'))
-    // animation: shake 0.5s;
 
+}
+function clicker(num) {
+    if (multiplier == 0) {
+        numbers = numbers + num;
+    } else {
+        numbers = numbers + num + multiplier;
+    }
+    document.getElementById('number').innerHTML = numbers;
 }
 /*
 *This function can be called to add a new worker
@@ -88,26 +71,50 @@ function clicker(num) {
 *then it adds a new worker to the worker variable and takes the price of a worker of the numbers
 *the else creates a new element which says you don't have enough money
 */
-function addWorker() {
+var workerCost = Math.floor(10 * Math.pow(1.1,workers));
+var grandmaCost = Math.floor(20 * Math.pow(1.1,grandmas));
+var bankerCost = Math.floor(30 * Math.pow(1.1,bankers));
+var clickCost = Math.floor(100 * Math.pow(1.1, multiplier));
 
-     var workerCost = Math.floor(10 * Math.pow(1.1,workers));
-     if(numbers >= workerCost){
-    workers = workers + 1;
-    numbers = numbers - workerCost;
-    document.getElementById('workers').innerHTML = workers;
-    document.getElementById('number').innerHTML = numbers;
-        } else {
-            var h2= document.createElement('h2');
-            h2.setAttribute('id','error');
-            var t = document.createTextNode('je hebt niet genoeg geld');
-            h2.appendChild(t);
-            document.body.appendChild(h2);
+window.setInterval(function () {
+    if (numbers < workerCost) {
+        document.getElementById('worker').style.color = 'grey';
+    } else {
+        document.getElementById('worker').style.color = '';
+    }
+    if (numbers < grandmaCost){
+        document.getElementById('grandma').style.color = 'grey';
+    } else {
+        document.getElementById('grandma').style.color = '';
+    }
+    if (numbers < bankerCost) {
+        document.getElementById('banker').style.color = 'grey';
+    } else {
+        document.getElementById('banker').style.color = '';
+    }
+    if (numbers < clickCost) {
+        document.getElementById('click').style.color = 'grey';
+    } else {
+        document.getElementById('click').style.color = '';
+    }
+}, 1000);
+
+
+function addWorker() {
+    var workerCost = Math.floor(10 * Math.pow(1.1,workers));
+    if(numbers >= workerCost){
+        workers = workers + 1;
+        numbers = numbers - workerCost;
+        document.getElementById('workers').innerHTML = workers;
+        document.getElementById('number').innerHTML = numbers;
+    } else {
+        console.log('hello')
+        // document.getElementById('worker').style.color = 'grey';
         }
-        let workerTotal =  workers;
-        var nextCost = Math.floor(10 * Math.pow(1.1,workers));
-        document.getElementById('workerCost').innerHTML = nextCost;
-        document.getElementById('workerTotal').innerHTML = workerTotal;
-            // console.log(workers);
+    let workerTotal =  workers;
+    var nextCost = Math.floor(10 * Math.pow(1.1,workers));
+    document.getElementById('workerCost').innerHTML = nextCost;
+    document.getElementById('workerTotal').innerHTML = workerTotal;
         }
 /*
 *This is the same ass adding a worker without the error
@@ -128,7 +135,7 @@ function addGrandma() {
 }
 
 function addBanker() {
-    var bankerCost = Math.floor(30 * Math.pow(1.1,bankers))
+    var bankerCost = Math.floor(30 * Math.pow(1.1,bankers));
     if (numbers >= bankerCost) {
         bankers = bankers + 1;
         numbers = numbers - bankerCost;
@@ -140,9 +147,28 @@ function addBanker() {
     document.getElementById('bankerCost').innerHTML = nextCost;
     document.getElementById('bankerTotal').innerHTML = bankerTotal;
     // console.log('new banker');
+    //bankertotal shows in the upgrades tab how much money you get from the bankers.
 }
-    console.log(bankers);
 
+function upgradeCLick() {
+    var clickCost = Math.floor(100 * Math.pow(2.1, multiplier));
+    if (numbers >= clickCost) {
+        multiplier = multiplier + 1;
+        numbers = numbers - clickCost;
+        document.getElementById('clicks').innerHTML = multiplier;
+        document.getElementById('number').innerHTML = numbers;
+    }
+    var nextCost = Math.floor(100 * Math.pow(2.1, multiplier));
+    document.getElementById('clickCost').innerHTML = nextCost;
+}
+// console.log(bankerCost);
+// (function () {
+//     if (numbers < bankerCost) {
+//         console.log('you aint got no money boy')
+//     } else {
+//         console.log('yes')
+//     }
+// }) ();
     // function multiplier() {
     //     if (grandmas >= 2) {
     //         for (var i = 0; i < 10; i++) {
@@ -157,16 +183,16 @@ function addBanker() {
     // }
 
      window.setInterval(function() {
-        clicker(workers)
+        autoClicker(workers)
         for (var i = 0; i < 5; i++) {
-            clicker(bankers)
+            autoClicker(bankers)
         }
 
         for (var i = 0; i < 2; i++) {
-            clicker(grandmas)
+            autoClicker(grandmas)
         }
         // multiplier();
-        let total = workers +(grandmas * 2) + (bankers * 5);
+        let total = workers + (grandmas * 2) + (bankers * 5);
         document.getElementById('moneyPerSecond').innerHTML = total;
     }, 1000);
 
@@ -181,3 +207,62 @@ function addBanker() {
     window.setInterval(function() {
         saveData();
     }, 60000);
+
+
+    //Saving the game and other save options:
+    function saveData() {
+        var save = {
+            numbers : numbers,
+            workers: workers,
+            grandmas : grandmas,
+            bankers : bankers,
+            multiplier : multiplier
+        }
+     localStorage.setItem("save", JSON.stringify(save));
+     console.log('saved');
+    }
+    //listens to a browser closing or reloading and calls the save function
+    window.addEventListener("beforeunload", function(e){
+        saveData();
+    }, false);
+
+    //checks if the ctrl + s button are entered and then calls the save function
+    var isCtrl = false;$(document).keyup(function (e) {
+    if(e.which == 17) isCtrl=false;
+    }).keydown(function (e) {
+        if(e.which == 17) isCtrl=true;
+        if(e.which == 83 && isCtrl == true) {
+            var save = {
+                numbers : numbers,
+                workers: workers,
+                grandmas : grandmas,
+                bankers : bankers,
+                multiplier : multiplier
+            }
+            alert('saved');
+            localStorage.setItem("save", JSON.stringify(save));
+
+     	return false;
+     }
+    });
+
+    /*this is a function that loads the saved data from the localstorage and puts the data into the variables declared up above.
+    *this function is automatically called when the browser is reloaded
+    */
+    // (function loadData() {
+    // const savegame = JSON.parse(localStorage.getItem("save"));
+    // if (typeof savegame.numbers !== "undefined") numbers = savegame.numbers;
+    // if (typeof savegame.workers !== "undefined") workers = savegame.workers;
+    // if (typeof savegame.grandmas !== "undefined") grandmas = savegame.grandmas;
+    // if (typeof savegame.bankers !== "undefined") bankers = savegame.bankers;
+    // if (typeof savegame.multiplier !== "undefined") multiplier = savegame.multiplier;
+    //
+    // console.log('loaded');
+    // })();
+    /*
+    *this function deletes the data from the localStorage
+    */
+    function deleteData() {
+        localStorage.removeItem("save");
+        console.log('save deleted');
+    }
