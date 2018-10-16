7c80e72526906141a6f0e717abe258d7598cb8c1
Alex Kalis <99039736@mydavinci.nl>
2018-10-16 18:23:34 +0200

    Added being able to prestige. Worked with music.

--- index.js
+++ index.js
@@ -4,14 +4,84 @@ let workers = 0;
 let grandmas = 0;
 let bankers = 0;
 let multiplier = 0;
+let prestiges = 0;
+let gold = 0;
 document.getElementById("settings").style.visibility = "hidden";
 document.getElementById("info").style.visibility = "hidden";
 document.getElementById("stats").style.visibility = "hidden";
+document.getElementById('prestige').style.visibility = 'hidden';
+
+//Saving the game and other save options:
+function saveData() {
+    var save = {
+        numbers : numbers,
+        workers: workers,
+        grandmas : grandmas,
+        bankers : bankers,
+        multiplier : multiplier
+    }
+ localStorage.setItem("save", JSON.stringify(save));
+ console.log('saved');
+}
+function savePrestige() {
+    var prestigeSave = {
+        prestiges : prestiges,
+        gold : gold
+    }
+    localStorage.setItem("prestigeSave", JSON.stringify(prestigeSave));
+
+}
+//listens to a browser closing or reloading and calls the save function
+// window.addEventListener("beforeunload", function(e){
+//     saveData();
+// }, false);
+
+//checks if the ctrl + s button are entered and then calls the save function
+var isCtrl = false;$(document).keyup(function (e) {
+if(e.which == 17) isCtrl=false;
+}).keydown(function (e) {
+    if(e.which == 17) isCtrl=true;
+    if(e.which == 83 && isCtrl == true) {
+        var save = {
+            numbers : numbers,
+            workers: workers,
+            grandmas : grandmas,
+            bankers : bankers,
+            multiplier : multiplier
+        }
+        alert('saved');
+        localStorage.setItem("save", JSON.stringify(save));
+
+    return false;
+ }
+});
+
+/*this is a function that loads the saved data from the localstorage and puts the data into the variables declared up above.
+*this function is automatically called when the browser is reloaded
+*/
 
 
+function deleteData() {
+    localStorage.removeItem("save");
+    console.log('save deleted');
+}
+window.onload = function loadData() {
+    const savegame = JSON.parse(localStorage.getItem("save"));
+    if (savegame == null) {
+        console.log('no save');
+    } else{
+    if (typeof savegame.numbers !== "undefined") numbers = savegame.numbers;
+    if (typeof savegame.workers !== "undefined") workers = savegame.workers;
+    if (typeof savegame.grandmas !== "undefined") grandmas = savegame.grandmas;
+    if (typeof savegame.bankers !== "undefined") bankers = savegame.bankers;
+    if (typeof savegame.multiplier !== "undefined") multiplier = savegame.multiplier;
+    console.log('loaded');
+}
+}
 
-//saving the game in local storage
-
+/*
+*this function deletes the data from the localStorage
+*/
 
 
 function settings() {
@@ -83,7 +153,7 @@ function closeStats() {
 function autoClicker(num) {
     numbers = numbers + num;
     document.getElementById('number').innerHTML = numbers;
-    document.getElementById('numbers').innerHTML = numbers;
+    document.getElementById('numbersStats').innerHTML = numbers;
     document.title= "Clicker €" + numbers;
 
 }
@@ -94,7 +164,7 @@ function clicker(num) {
         numbers = numbers + num + multiplier;
     }
     document.getElementById('number').innerHTML = numbers;
-    document.getElementById('numbers').innerHTML = numbers;
+    document.getElementById('numbersStats').innerHTML = numbers;
     document.title= "Clicker €" + numbers;
 }
 /*
@@ -108,28 +178,7 @@ var grandmaCost = Math.floor(20 * Math.pow(1.1,grandmas));
 var bankerCost = Math.floor(30 * Math.pow(1.1,bankers));
 var clickCost = Math.floor(100 * Math.pow(1.1, multiplier));
 
-window.setInterval(function () {
-    if (numbers < workerCost) {
-        document.getElementById('worker').style.color = 'grey';
-    } else {
-        document.getElementById('worker').style.color = '';
-    }
-    if (numbers < grandmaCost){
-        document.getElementById('grandma').style.color = 'grey';
-    } else {
-        document.getElementById('grandma').style.color = '';
-    }
-    if (numbers < bankerCost) {
-        document.getElementById('banker').style.color = 'grey';
-    } else {
-        document.getElementById('banker').style.color = '';
-    }
-    if (numbers < clickCost) {
-        document.getElementById('click').style.color = 'grey';
-    } else {
-        document.getElementById('click').style.color = '';
-    }
-}, 1000);
+
 
 
 function addWorker() {
@@ -137,12 +186,11 @@ function addWorker() {
     if(numbers >= workerCost){
         workers = workers + 1;
         numbers = numbers - workerCost;
+        console.log(workers);
         document.getElementById('workers').innerHTML = workers;
         document.getElementById('number').innerHTML = numbers;
-    } else {
-        console.log('hello')
+    }
         // document.getElementById('worker').style.color = 'grey';
-        }
     let workerTotal =  workers;
     var nextCost = Math.floor(10 * Math.pow(1.1,workers));
     document.getElementById('workerCost').innerHTML = nextCost;
@@ -214,6 +262,64 @@ function upgradeCLick() {
     //     }
     // }
 
+    //starting with Prestige
+    window.setInterval(function() {
+            if (numbers >= 1000) {
+           alert('You can prestige');
+           document.getElementById('prestige').style.visibility = 'visible';
+       }
+   }, 10000);
+
+
+
+    function prestige() {
+        prestiges = prestiges + 1;
+        var howMuchGold =  Math.floor(1 * Math.pow(1.1,prestiges));
+        gold = gold + howMuchGold;
+        console.log(prestiges);
+        if (document.getElementById('prestige').onclick) {
+            document.getElementById('prestige').style.visibility = 'hidden';
+            localStorage.removeItem("save");
+            location.reload();
+        }
+        savePrestige();
+        // console.log(prestigeAble);
+        console.log(`You have ${gold} gold`);
+        console.log('You prestiged how many times? ');
+        console.log(`Oh ${prestiges} time(s)`)
+    }
+    // var prestigeAble =  Math.floor(10000 * Math.pow(1.1,prestiges));
+
+
+    // if (numbers >= prestigeAble) {
+    //     alert('You can prestige');
+    // } else {
+    //     document.getElementById('prestige').style.visibility = 'hidden';
+    // }
+
+    //
+    window.setInterval(function () {
+        if (numbers < workerCost) {
+            document.getElementById('worker').style.color = 'grey';
+        } else {
+            document.getElementById('worker').style.color = '';
+        }
+        if (numbers < grandmaCost){
+            document.getElementById('grandma').style.color = 'grey';
+        } else {
+            document.getElementById('grandma').style.color = '';
+        }
+        if (numbers < bankerCost) {
+            document.getElementById('banker').style.color = 'grey';
+        } else {
+            document.getElementById('banker').style.color = '';
+        }
+        if (numbers < clickCost) {
+            document.getElementById('click').style.color = 'grey';
+        } else {
+            document.getElementById('click').style.color = '';
+        }
+    }, 100);
      window.setInterval(function() {
         autoClicker(workers)
         for (var i = 0; i < 5; i++) {
@@ -240,64 +346,23 @@ function upgradeCLick() {
         saveData();
     }, 60000);
 
+    //audio
+    // var music = document.createElement('audio');
+ //    var audio = document.querySelector('audio');
+ //    console.log(audio   );
+ //    function SetVolumeHalf()
+ // {
+ //     audio.volume = 0.5;
+ //     console.log('why')
+ // }
+ // function SetVolumeMute() {
+ //     audio.volume = 0.0;
+ // }
+ // function SetVolumeFull() {
+ //     audio.volume = 1;
+ // }
 
-    //Saving the game and other save options:
-    function saveData() {
-        var save = {
-            numbers : numbers,
-            workers: workers,
-            grandmas : grandmas,
-            bankers : bankers,
-            multiplier : multiplier
-        }
-     localStorage.setItem("save", JSON.stringify(save));
-     console.log('saved');
-    }
-    //listens to a browser closing or reloading and calls the save function
-    window.addEventListener("beforeunload", function(e){
-        saveData();
-    }, false);
-
-    //checks if the ctrl + s button are entered and then calls the save function
-    var isCtrl = false;$(document).keyup(function (e) {
-    if(e.which == 17) isCtrl=false;
-    }).keydown(function (e) {
-        if(e.which == 17) isCtrl=true;
-        if(e.which == 83 && isCtrl == true) {
-            var save = {
-                numbers : numbers,
-                workers: workers,
-                grandmas : grandmas,
-                bankers : bankers,
-                multiplier : multiplier
-            }
-            alert('saved');
-            localStorage.setItem("save", JSON.stringify(save));
-
-     	return false;
-     }
-    });
-
-    /*this is a function that loads the saved data from the localstorage and puts the data into the variables declared up above.
-    *this function is automatically called when the browser is reloaded
-    */
-    (function loadData() {
-    const savegame = JSON.parse(localStorage.getItem("save"));
-    if (typeof savegame.numbers !== "undefined") numbers = savegame.numbers;
-    if (typeof savegame.workers !== "undefined") workers = savegame.workers;
-    if (typeof savegame.grandmas !== "undefined") grandmas = savegame.grandmas;
-    if (typeof savegame.bankers !== "undefined") bankers = savegame.bankers;
-    if (typeof savegame.multiplier !== "undefined") multiplier = savegame.multiplier;
-
-    console.log('loaded');
-    })();
-    /*
-    *this function deletes the data from the localStorage
-    */
-    function deleteData() {
-        localStorage.removeItem("save");
-        console.log('save deleted');
-    }
-    document.getElementById('workers').innerHTML = workers;
-    document.getElementById('grandmas').innerHTML = grandmas;
-    document.getElementById('bankers').innerHTML = bankers;
+    // document.getElementById('numbersStats').innerHTML = numbers;
+    document.getElementById('workersStats').innerHTML = workers;
+    document.getElementById('grandmasStats').innerHTML = grandmas;
+    document.getElementById('bankersStats').innerHTML = bankers;
